b5cd6bb9f5fdd85b4bc27a39bc2b65e7d7131a97
MinseokGo <rhalstjr1999@naver.com>
2000-05-29 10:41:00 +0000

    update

--- src/main/java/ObjectOriented1/ReferenceParamEx3.java
+++ src/main/java/ObjectOriented1/ReferenceParamEx3.java
@@ -15,17 +15,15 @@ public class ReferenceParamEx3 {
 
     static void printAll(int[] arr) {
         for (int i : arr) {
-            System.out.print(i + ", ");
+            System.out.print(i + " ");
         }
         System.out.println();
     }
-    public void int main(String[] args) {
+    public static void main(String[] args) {
         int[] arr = new int[] { 3, 2, 1, 6, 5, 4 };
 
         printAll(arr);
-
         sortArr(arr);
         printAll(arr);
     }
-    str -> return str.length();
 }
